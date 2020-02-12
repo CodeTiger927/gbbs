399de37ec65e4063adc5139bb8efde3e03b74e6e
Alex Fan <codetiger927@gmail.com>
2020-02-12 00:18:51 -0500

    adding header file

--- benchmarks/SubgraphCounting/h-index.cc
+++ benchmarks/SubgraphCounting/h-index.cc
@@ -1,74 +1,18 @@
 using namespace std;
 
-#include <iostream>
-#include <map>
-#include <set>
-
-set<int> S;
-map<int,int> f;
-set<int> H;
-map<int,int> F;
-set<int> B;
-map<int,set<int>> C;
-
-int insert(int e,int x) {
-	F[x] = e;
-	f[e] = x;
-	C[x].insert(e);
-	if(x > H.size()) {
-		H.insert(e);
-		if(B.size() == 0) {
-			if(C[H.size() - 1].size() == 0) {
-				B = C[H.size()];
-				C[H.size()] = set<int>();
-			}
-		}else{
-			int cur = *(B.begin());
-			B.erase(B.begin());
-			H.erase(cur);
-			C[H.size()].insert(cur);
-		}
-	}
-	return H.size();
-}
-
-int erase(int e) {
-	F.erase(e);
-	if(B.find(e) != B.end()) {
-		B.erase(e);
-	}else{
-		C[f[e]].erase(e);
-	}
-	if(H.find(e) != H.end()) {
-		H.erase(e);
-		if(C[H.size() + 1].size() == 0) {
-			C[H.size() + 1] = B;
-			B = set<int>();
-		}else{
-			int cur = *(C[H.size() + 1].begin());
-			C[H.size() + 1].erase(cur);
-			B.insert(cur);
-			H.insert(cur);
-		}
-	}
-	return H.size();
-}
-
-int change(int e,int x) {
-	erase(e);
-	return insert(e,x);
-}
+#include "h-index.h"
 
 int main() {
-	cout << insert(1,4) << endl;
-	cout << insert(2,3) << endl;
-	cout << insert(3,4) << endl;
-	cout << insert(4,4) << endl;
-	cout << erase(2) << endl;
-	cout << insert(2,4) << endl;
-	cout << erase(2) << endl;
-	cout << insert(2,4) << endl;
-	cout << change(2,3) << endl;
-	cout << change(2,4) << endl;
+	HSet h;
+	cout << h.insert(1,4) << endl;
+	cout << h.insert(2,3) << endl;
+	cout << h.insert(3,4) << endl;
+	cout << h.insert(4,4) << endl;
+	cout << h.erase(2) << endl;
+	cout << h.insert(2,4) << endl;
+	cout << h.erase(2) << endl;
+	cout << h.insert(2,4) << endl;
+	cout << h.change(2,3) << endl;
+	cout << h.change(2,4) << endl;
 	return 0;
 }
---
+++ benchmarks/SubgraphCounting/h-index.h
@@ -0,0 +1,64 @@
+#include <iostream>
+#include <map>
+#include <set>
+#include "pbbslib/sample_sort.h"
+#include "pbbslib/monoid.h"
+#include "ligra/ligra.h"
+
+struct HSet {
+	std::set<int> S;
+	std::map<int,int> f;
+	std::set<int> H;
+	std::map<int,int> F;
+	std::set<int> B;
+	std::map<int,set<int>> C;
+
+	int insert(int e,int x) {
+		F[x] = e;
+		f[e] = x;
+		C[x].insert(e);
+		if(x > H.size()) {
+			H.insert(e);
+			if(B.size() == 0) {
+				if(C[H.size() - 1].size() == 0) {
+					B = C[H.size()];
+					C[H.size()] = set<int>();
+				}
+			}else{
+				int cur = *(B.begin());
+				B.erase(B.begin());
+				H.erase(cur);
+				C[H.size()].insert(cur);
+			}
+		}
+		return H.size();
+	}
+	
+	int erase(int e) {
+		F.erase(e);
+		if(B.find(e) != B.end()) {
+			B.erase(e);
+		}else{
+			C[f[e]].erase(e);
+		}
+		if(H.find(e) != H.end()) {
+			H.erase(e);
+			if(C[H.size() + 1].size() == 0) {
+				C[H.size() + 1] = B;
+				B = set<int>();
+			}else{
+				int cur = *(C[H.size() + 1].begin());
+				C[H.size() + 1].erase(cur);
+				B.insert(cur);
+				H.insert(cur);
+			}
+		}
+		return H.size();
+	}
+	
+	int change(int e,int x) {
+		erase(e);
+		return insert(e,x);
+	}
+};
+
