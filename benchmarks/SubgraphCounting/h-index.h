0da25e5851302c333481f8502300bb94461503b0
Alex Fan <codetiger927@gmail.com>
2020-03-02 18:41:57 -0500

    Add new Symetric Graph Structure

--- benchmarks/SubgraphCounting/h-index.h
+++ benchmarks/SubgraphCounting/h-index.h
@@ -4,15 +4,12 @@
 #include <limits>
 #include <vector>
 #include "ligra/ligra.h"
+#include "symetric_graph.h"
 #include "ligra/pbbslib/sparse_table.h"
 #include "pbbslib/hash_table.h"
 
 struct HSet {
-	// TODO: don't use a constant like 1000; it should be passed in
-	// Here, f actually is just the degree, so we don't need it at all; just pass
-	// in and save a copy of the graph, so HSet should be templatized
 	int f[1000];
-	// TODO: replace with GBBS
 	std::set<int> H;
 	std::set<int> P;
 	std::set<int> B;
@@ -78,16 +75,9 @@ struct HSet {
 };
 
 
-// TODO: don't call this graph -- it's confusing with the Graph templates
-// Separate out the way we store the graph from the triangle-counting code
-// Also, this isn't quite what we need? It seems like there's no separation of
-// H (H-index) and vertices not in H.
 struct graph {
 	public:
 		HSet h;
-		// TODO: don't use constants like these; should be malloc-ed
-		// using sequence would probably be easier than using malloc directly
-		// Also, edges should come from the passed-in Graph template object
 		std::vector<int> edges[1000];
 		std::vector<int> hedges[100][100];
 
@@ -102,24 +92,21 @@ struct graph {
 		}
 
 		int connect(int v,int u) {
-			// Add u and v to H
 			h.inc(v);
 			h.inc(u);
 
 			triangles += hedges[u][v].size();
 			for(int i = 0;i < edges[u].size();i++) {
-				// Add new wedge v -- u -- i
 				hedges[v][edges[u][i]].push_back(u);
 				hedges[edges[u][i]][v].push_back(u);
 			}
 			for(int i = 0;i < edges[v].size();i++) {
-				// Add new wedge u -- v -- i
 				hedges[u][edges[v][i]].push_back(v);
 				hedges[edges[v][i]][u].push_back(v);
 			}
 			edges[v].push_back(u);
 			edges[u].push_back(v);
 
-			return triangles;
+			return this -> triangles;
 		}
 };
