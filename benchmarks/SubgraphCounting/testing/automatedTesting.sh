40315ff198f1ac9383cabf190c41eb410e936556
Alvin <alvinlu2023@gmail.com>
2020-12-30 09:53:06 -0500

    Testing

--- benchmarks/SubgraphCounting/testing/automatedTesting.sh
+++ benchmarks/SubgraphCounting/testing/automatedTesting.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #After putting in the info, use the command
-#./automatedTesting.sh|sed '/^#/d'|sed '/^D/d'|sed '/^T/d'|sed '/^$/d'>./results/res_GRAPHNAME.txt
+#./automatedTesting.sh
 #Change GRAPHNAME to the graph name(like DBLP)
 #If you don't have the results folder, please use the command "mkdir results"
 
@@ -15,7 +15,7 @@ for a in ${types[@]}; do
 	for b in ${batchesSize[@]}; do
 		for c in ${parameter2[@]}; do
 			for d in ${useP[@]}; do
-				../SubgraphCounting -s -rounds 3 -type $a -size $b -edges $c -nodes $nodes -P $d -scriptMode 1 $fname
+				../SubgraphCounting -s -rounds 3 -type $a -size $b -edges $c -nodes $nodes -P $d -scriptMode 1 $fname |sed '/^#/d'|sed '/^D/d'|sed '/^T/d'|sed '/^$/d'>>./results/res_DBLP.txt
 			done
 		done
 	done
