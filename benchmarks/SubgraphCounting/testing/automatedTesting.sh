abad11628c2c125b562bd5c5cf09b9d0bdde2eef
Alex Fan <codetiger927@gmail.com>
2020-12-30 16:46:51 -0500

    More minor fixes

--- benchmarks/SubgraphCounting/testing/automatedTesting.sh
+++ benchmarks/SubgraphCounting/testing/automatedTesting.sh
@@ -5,7 +5,7 @@
 #If you don't have the results folder, please use the command "mkdir results"
 
 fname="../inputs/graph_test_DBLP.txt"
-types=(0, 1)
+types=(0)
 nodes=20000
 parameter2=(5)
 batchesSize=(10, 20, 30, 40, 50)
@@ -15,7 +15,7 @@ for a in ${types[@]}; do
 	for b in ${batchesSize[@]}; do
 		for c in ${parameter2[@]}; do
 			for d in ${useP[@]}; do
-				../SubgraphCounting -s -rounds 3 -type $a -size $b -edges $c -nodes $nodes -P $d -scriptMode 1 $fname |sed '/^#/d'|sed '/^D/d'|sed '/^T/d'|sed '/^$/d'>>./results/res_DBLP.txt
+				../SubgraphCounting -s -rounds 3 -type $a -size $b -edges $c -nodes $nodes -useP $d -scriptMode 1 $fname |sed '/^#/d'|sed '/^D/d'|sed '/^T/d'|sed '/^$/d'>>./results/res_DBLP.txt
 			done
 		done
 	done
